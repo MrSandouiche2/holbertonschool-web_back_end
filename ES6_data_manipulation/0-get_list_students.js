f2d8446ae6d09888a28c500f34a8fe2f22f86450
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-11-27 12:59:42 +0100

    pushed 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,17 +1,10 @@
-export default function getListStudents() {
-  return [{
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  },
-  {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  },
-  {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  }];
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
+
+const students = getListStudents();
+console.log(students); // eslint-disable-line no-console
