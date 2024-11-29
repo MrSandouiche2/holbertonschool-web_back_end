b977191032692b3844e81691053f4647e6ae7256
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-11-29 23:30:32 +0100

    pushed all

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+import divideFunction from './8-try';
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
