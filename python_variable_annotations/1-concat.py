7fbf05cb0315c38b39909a05dc922e553b14157d
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-12-05 15:13:49 +0100

    pushed 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""demarrage python"""
+
+def concat(str1: str, str2: str) -> str:
+    """concat fonction
+
+va concatener
+    Returns:
+        va concatener le sum
+    """
+    return str1 + str2
