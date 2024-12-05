f526dbfddbc518de04e44d99c465c8aef96d3162
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-12-05 14:52:47 +0100

    added docs

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""fonction"""
 
 def add(a: float, b: float) -> float:
+    """
+    will return the sum of the float
+
+    Returns:
+        float: return the sum of a+b
+    """
     return a + b
